47794cfc8582d9c7cd40ef89775d3778548545a1
critical-path <37241479+critical-path@users.noreply.github.com>
2019-09-16 19:51:47 -0400

    Update `start-api-and-app.sh`.

--- start-api-and-app.sh
+++ start-api-and-app.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 echo -e "Starting the API in the background...\n"
-gunicorn --bind=0.0.0.0:8079\
-         --workers=2\
-         "stakeholders.api:create_api()" &
+gunicorn --bind=0.0.0.0:8079 --workers=2 "stakeholders.api:create_api()" &
 
 echo -e "Starting the app in the foreground...\n"
-gunicorn --bind=0.0.0.0:8080\
-         --workers=2\
-         "stakeholders.app:create_app()"
+gunicorn --bind=0.0.0.0:8080 --workers=2 "stakeholders.app:create_app()"
